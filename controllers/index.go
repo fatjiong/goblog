e8947e5d670c41eec9338ef11326b639504ff26e
Jonni <jonni@iv66.com>
2018-11-14 11:29:13 +0800

    [+] 完全嵌入gorm

--- controllers/index.go
+++ controllers/index.go
@@ -1,10 +1,27 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/fatjiong/goblog/model"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-func IndexGet(c *gin.Context){
-	c.HTML(http.StatusOK, "index.html",nil)
+// 首页控制器
+func IndexGet(c *gin.Context) {
+	//分类列表
+	categoryList, err := model.GetCategoryList(0)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//推荐文章列表
+	recommendList, err := model.GetArticleRecommend()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"categoryList":  categoryList,
+		"recommendList": recommendList,
+	})
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"github.com/fatjiong/goblog/model"
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
@@ -14,34 +13,34 @@ func main() {
 	//router.LoadHTMLGlob("views/*")
 	//router.GET("/", controllers.IndexGet)
 
-	db, err := gorm.Open("mysql", "root:123456@/gblog?charset=utf8mb4&parseTime=True&loc=Local")
+	//初始化数据库
+	db, err := model.InitDB()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	//categoryList, err := model.GetCategoryList(0)
+	//
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//
+	//for _, category := range categoryList {
+	//	fmt.Println(category.Name)
+	//}
+
+	//生成点数据
+	//model.GenerateData()
+
+	articleList, err := model.GetArticleRecommend()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	//设置默认表名前缀
-	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
-		return "gb_" + defaultTableName
+	for _, article := range articleList {
+		fmt.Println(article.Title + "_" + article.Author)
 	}
-
-	//关闭数据库
-	defer db.Close()
-
-	//初始化数据库
-	db.AutoMigrate(&model.Category{}, &model.Article{}, &model.Adver{}, &model.Comment{}, &model.Feedback{}, &model.Sharelink{})
-	//
-	//for i := 1; i < 11; i++ {
-	//	db.Create(&model.Category{
-	//		Name: "测试分类" + strconv.Itoa(i),
-	//		Pid:  0,
-	//	})
-	//	for j := 1; j < 11; j++ {
-	//		db.Create(&model.Category{
-	//			Name: "测试分类" + strconv.Itoa(i) + "_" + strconv.Itoa(j),
-	//			Pid:  uint(i),
-	//		})
-	//	}
-	//}
 
 	////发布文章页面
 	//router.GET("/article/new/", controller.NewArticle)
